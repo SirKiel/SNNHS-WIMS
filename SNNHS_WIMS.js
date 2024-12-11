a16ce4b5a7e2f9a768e83884ffdc8fa0bcba1ff3
SirKiel <michaeljohnbalantac09@gmail.com>
2024-12-11 16:48:58 +0800

    Add files via upload

---
+++ SNNHS_WIMS.js
@@ -0,0 +1,18 @@
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  let b = today.getMonth();
+  let d = today.getDate();
+  let y = today.getFullYear();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =  h + ":" + m + ":" + " | " + b + " " + d + ", " + y;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
