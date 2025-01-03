b75f7252a6be90e954f570f945d98fc4ddc1c277
SirKiel <michaeljohnbalantac09@gmail.com>
2025-01-03 15:34:52 +0800

    $git commit -m "Updated CSS files for Student and Supervisor Profile" ````

--- Supervisor/SNNHS_WIMS_Supervisor_Profile.php
+++ Supervisor/SNNHS_WIMS_Supervisor_Profile.php
@@ -18,22 +18,117 @@
             <div class="main_panel">
                 <!--This is the main part-->
                 <div class="SupervisorProfile">
-                    <div class="NameID">
+                <div class="NameID">
                         <div>
                             <img src="../Assets/ProfileLogo.png">
                         </div>
                         <div id="SupervisorName">
-                            <h1>Juan De La Cruz</h1>
+                            <h1 id="Complete_Name">Juan De La Cruz</h1>
+                            <label id="Grade_Section">Manager</label><br>
                         </div>
-                        <hr>
                     </div>
+                    <hr>
                     <div class="BasicInformation">
-                        The content of this part will be updated soon.
+                        <div class="Position">
+                            <label>Position</label>
+                            <label class="InfoBox" id="POSITION">Work Immersion Supervisor</label>
+                        </div>
+                        <div class="Learner_Sex">
+                            <label>Sex</label>
+                            <label class="InfoBox" id="SEX">Male/Female</label> 
+                        </div>
+                        <div class="Learner_BD">
+                            <label>Birthdate</label>
+                            <label class="InfoBox" id="BIRTH_DATE">MM/DD/YYYY</label>
+                        </div>
+                        <div>
+                            <label>Contact Number</label>
+                            <label class="InfoBox" id="CONTACT_NUMBER">09123456789</label>
+                        </div>
+                        <div>
+                            <label>Address</label>
+                            <label class="InfoBox" id="ADDRESS">Brgy. XX, XXXXXXX, Ilocos Norte</label>
+                        </div>
+                        </br>
+                        <div>
+                            <label>Company</label>
+                            <label class="InfoBox" id="COMPANY">XXXXXXXXXXXXXXXXXXXXX</label>
+                        </div>
+                        <div>
+                            <label>Department</label>
+                            <label class="InfoBox" id="DEPARTMENT">XXXXXXXXXXXXXXXXXXXXX</label>
+                        </div>
                     </div>
                     <hr>
                     <div class="ProfileButtons">
-                        <button name="edit_profile" id="EditProfile">Edit Profile</button>
+                        <button name="edit_profile" id="EditProfile" onclick="openWindow()">Edit Profile</button>
                     </div>
+                </div>
+            </div>
+        </div>
+
+        <!--This is where a pop-up will appear when buttons are clicked-->
+        <div id="PopUp">
+            <div class="PopUpContainer">
+                <div class="PopUpHeader">
+                    <span class="close" onclick="closeWindow()">&times;</span>
+                </div>
+                <hr>
+                <form>
+                    <div class="PopUpContent">
+                        <div class="SupervisorProfile">
+                            <div class="BasicInformation">
+                                <div class="NameID">
+                                    <label>Edit your Profile</label><br>
+                                </div>
+                                <div id="StudentName">
+                                    <label>Last Name</label>
+                                    <input type="text" class="InfoBox" id="LAST_NAME" placeholder="Enter your last name">
+                                    <label>First Name</label>
+                                    <input type="text" class="InfoBox" id="FIRST_NAME" placeholder="Enter your first name">
+                                    <label>Middle Name</label>
+                                    <input type="text" class="InfoBox" id="MIDDLE_NAME" placeholder="Enter your middle name">
+                                    <label>Position</label>
+                                    <input type="text" class="InfoBox" id="POSITION" placeholder="Enter your position">
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="BasicInformation">
+                                <!--<div>
+                                    <label>Learner's Reference Number</label>
+                                    <input type="text" class="InfoBox" id="LRN" placeholder="Enter your LRN">
+                                </div>-->
+                                <div>
+                                    <label>Sex</label>
+                                    <input type="radio" class="Radio" id="SEX" name="Sex" value="MALE"><label style="display:inline-block;">Male</label>
+                                    <input type="radio" class="Radio" id="SEX" name="Sex" value="FEMALE"><label style="display:inline-block;">Female</label>
+                                </div>
+                                <div>
+                                    <label>Birthdate</label>
+                                    <input type="Date" class="InfoBox" id="BIRTH_DATE">
+                                </div>
+                                <div>
+                                    <label>Contact Number</label>
+                                    <input type="number" class="InfoBox" id="CONTACT_NUMBER" placeholder="Enter your contact number">
+                                </div>
+                                <div>
+                                    <label>Address</label>
+                                    <input type="text" class="InfoBox" id="ADDRESS" placeholder="Enter your Address">
+                                </div>
+                                <div>
+                                    <label>Company</label>
+                                    <input type="text" class="InfoBox" id="COMPANY" placeholder="Enter Company name">
+                                </div>
+                                <div>
+                                    <label>Department</label>
+                                    <input type="text" class="InfoBox" id="DEPARTMENT" placeholder="Enter Department">
+                                </div>
+                            <hr>
+                            <div class="ProfileButtons">
+                                <button name="save_profile" id="saveProfile">Save Changes</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
