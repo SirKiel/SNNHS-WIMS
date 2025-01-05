f49518511b03e0a05b80ba3dc67ac162923e7975
SirKiel <michaeljohnbalantac09@gmail.com>
2025-01-05 22:02:26 +0800

    $git commit -m "Update on FooterPanel Content and Design"

--- Assets/include/FooterPanel.php
+++ Assets/include/FooterPanel.php
@@ -7,5 +7,8 @@
         <p>Brgy. 24, San Nicolas, Ilocos Norte, 2901</p>
         <p>Telephone No.: 774-8665</p>
         <p>snnhs.highschool@gmail.com</p>
-        </div>
+    </div>
+    <div class="FooterCopyright">
+        <label>Copyright &copy; 2025 | San Nicolas National High School</label>
+    </div>
 </div>
