091e729a16de3d06d451ca0443b05d7b5e577f6c
SirKiel <michaeljohnbalantac09@gmail.com>
2025-01-26 20:23:53 +0800

    $git commit -m "Added a Logout button tot he header" ````

--- Assets/include/GroupManagerHeaderPanel.php
+++ Assets/include/GroupManagerHeaderPanel.php
@@ -1,5 +1,9 @@
 <div class="header_panel_GroupManager">
     <!--This is the header part-->
+    <div class="LogOut">
+        <a href="SNNHS_WIMS_Login.php"><img src="../Assets/LogOutLogo.png" class="LogOutIcon"></a>
+    </div>
+
     <center>
         <a href="SNNHS_WIMS_GroupManagers_Dashboard.php"><img class ="headerlogo" src="../Assets/Flourish Logo.png"></a>
         <h1>Work Immersion Monitoring System</h1>
