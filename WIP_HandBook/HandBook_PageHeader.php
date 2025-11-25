a7d4fb35ee40929197ede510de351145b290c397
SirKiel <michaeljohnbalantac09@gmail.com>
2025-11-25 09:26:53 +0800

    HandBook Header updated

---
+++ WIP_HandBook/HandBook_PageHeader.php
@@ -0,0 +1,10 @@
+    <div class="PageHeader">
+        <img src="../Assets/Department_of_Education_Logo.png">
+        <p class="Rep_PH">Republic of the Philippines</p>
+        <p class="DepEd">Department of Education</p>
+        <p class="Trajan">Region 1</p>
+        <p class="Trajan">SCHOOLS DIVISION OF ILOCOS NORTE</p>
+        <p class="Trajan">SAN NICOLAS ANTIONAL HIGH SCHOOL</p>
+        <p class="Trajan">SAN NICOLAS, ILOCOS NORTE</p>
+        <hr>
+    </div>
