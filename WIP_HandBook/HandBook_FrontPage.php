079b5008ad06680e63f8a06e30488f905c33f686
SirKiel <michaeljohnbalantac09@gmail.com>
2025-11-25 09:41:28 +0800

    Updated HandBook

--- WIP_HandBook/HandBook_FrontPage.php
+++ WIP_HandBook/HandBook_FrontPage.php
@@ -1,14 +1,5 @@
 <div class="PageSize">
-    <div class="PageHeader">
-        <img src="">
-        <p class="Rep_PH">Republic of the Philippines</p>
-        <p class="DepEd">Department of Education</p>
-        <p class="Trajan">Region 1</p>
-        <p class="Trajan">SCHOOLS DIVISION OF ILOCOS NORTE</p>
-        <p class="Trajan">SAN NICOLAS ANTIONAL HIGH SCHOOL</p>
-        <p class="Trajan">SAN NICOLAS, ILOCOS NORTE</p>
-        <hr>
-    </div>
+
     <!--Two Line Breakes-->
     <br>
     <br>
